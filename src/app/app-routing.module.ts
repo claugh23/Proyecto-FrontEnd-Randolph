d98f1e65633582704b6035d686629a174d8ac4ec
Claudio-Josue-Gonzalez-Hernandez <Claudio.Josue.Gonzalez.Hernandez@ibm.com>
2022-09-18 09:42:43 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'Inicio',component:LoginComponent},
+  {path:'/', redirectTo:'Inicio',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
